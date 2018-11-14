deb3e93eb3d42ef44b84d57f7c6f448f4e256a4d
Jason Nelson <Jasonn318@gmail.com>
2018-11-14 10:30:56 -0600

    add dna.rb

---
+++ easy1/dna.rb
@@ -0,0 +1,20 @@
+class DNA
+  attr_reader :dna_sequence
+
+  def initialize(dna)
+    @dna_sequence = dna 
+  end
+
+  def hamming_distance(other_genetic_makeup)
+    index = 0
+    hamming_distance = 0
+    dna_length = dna_sequence.length
+
+    other_genetic_makeup[0, dna_length].chars.each do |nucleobase|
+      hamming_distance += 1 unless dna_sequence[index] == nucleobase
+      index += 1
+    end
+
+    hamming_distance
+  end
+end
