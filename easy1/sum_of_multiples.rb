e9a277f985f21cbdeab518da4d373f6557e5a733
Jason Nelson <Jasonn318@gmail.com>
2018-11-28 14:16:58 -0600

    rubocop refactoring

--- easy1/sum_of_multiples.rb
+++ easy1/sum_of_multiples.rb
@@ -1,12 +1,13 @@
-# Write a program that, given a number, can find the sum of all the multiples of particular numbers up to but 
+# rubocop:disable Metrics/LineLength
+
+# Write a program that, given a number, can find the sum of all the multiples of particular numbers up to but
 # not including that number.
 
 # If we list all the natural numbers up to but not including 20 that are multiples of either 3 or 5,
 # we get 3, 5, 6, 9, 10, 12, 15, and 18. The sum of these multiples is 78.
 
-# Write a program that can find the sum of the multiples of a given set of numbers. 
+# Write a program that can find the sum of the multiples of a given set of numbers.
 # If no set of numbers is given, default to 3 and 5.
-
 
 # Set_Multiples: set which multiples are used by explicitly passing them one by one into :new
 #              : if no arguments are passed to new, us multiples 3 and 5
@@ -18,6 +19,8 @@
 #   multiples
 # verb
 #   sum_multiples
+
+# rubocop:enable Metrics/LineLength
 
 class SumOfMultiples
   def initialize(*multiples)
